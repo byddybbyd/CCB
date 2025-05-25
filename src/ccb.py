3be7c47176941a79c3c7832b5838724decf1ee4c
byddybbyd <178136455+byddybbyd@users.noreply.github.com>
2025-05-25 15:15:01 +0800

    upd ccb.py

--- src/ccb.py
+++ src/ccb.py
@@ -1,5 +1,15 @@
-print('   _|_|_|    _|_|_|  _|_|_|    ')
-print(' _|        _|        _|    _|  ')
-print(' _|        _|        _|_|_|    ')
-print(' _|        _|        _|    _|  ')
-print('   _|_|_|    _|_|_|  _|_|_|    ')
+from time import sleep
+
+for i in range(10):
+    print('   CCBCCB      CCBCCB   CCBCCB')
+    sleep(1)
+    print('CCB         CCB         CCB   CCB')
+    sleep(1)
+    print('CCB         CCB         CCBCCB')
+    sleep(1)
+    print('CCB         CCB         CCB   CCB')
+    sleep(1)
+    print('   CCBCCB      CCBCCB   CCBCCB')
+    sleep(1)
+    print()
+    sleep(1)
